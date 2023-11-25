400cca6e9a6dcc3e7ce1ff447a2c61113ede754c
Ahmed Akash <110014244+codesky7@users.noreply.github.com>
2023-11-25 20:27:45 +0600

    ANIME

---
+++ app/action.ts
@@ -0,0 +1,10 @@
+'use server';
+
+import console from "console";
+import exp from "constants";
+
+ export const fetchAnime=async(page: number)=>{
+    const response=await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`);
+    const data=await response.json();
+    return data;
+ }
